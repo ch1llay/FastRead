cae6fa0637a7ebc46f2f75951222462912881cf2
ch1llay <ch1llay@yandex.ru>
2021-11-07 18:58:47 +0300

    Добавьте файлы проекта.

---
+++ FastRead/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace FastRead
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.WordInMinute = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.WordInMinute)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(398, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите текст для чтения в поле ниже";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(2, 51);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(951, 610);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(772, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Запустить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.WordInMinute.Location = new System.Drawing.Point(489, 18);
+            this.WordInMinute.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.WordInMinute.Minimum = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            this.WordInMinute.Name = "numericUpDown1";
+            this.WordInMinute.Size = new System.Drawing.Size(120, 31);
+            this.WordInMinute.TabIndex = 3;
+            this.WordInMinute.Value = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.WordInMinute);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.WordInMinute)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown WordInMinute;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FastRead/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FastRead
+{
+    public partial class Form1 : Form
+    {
+        bool isStart = false;
+        int currentIndex = 0;
+        int currentWordIndex = 0;
+        string currentWord;
+        int textSize = 0;
+        string[] words;
+        float wordInLine;
+        float secForLine = 1;
+        float wordInMinute;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isStart = !isStart;
+            
+            button1.Text = isStart ? "Остановить" : "Запустить";
+            textSize = richTextBox1.Text.Length;
+            timer1.Enabled = isStart;
+            words = richTextBox1.Text.Split(' ');
+            wordInLine = richTextBox1.Lines[0].Split(' ').Length;
+            wordInMinute = WordInMinute.Value;
+        }
+
+        private void ColorWord(string text, Color color)
+        {
+            int position_save = richTextBox1.SelectionStart; // сохраняем позицию курсора из начально
+
+
+            richTextBox1.SelectionStart = 0;
+            richTextBox1.SelectionLength = textSize;
+            richTextBox1.SelectionColor = Color.Black;
+            richTextBox1.SelectionStart = position_save;
+
+            string str = text;
+
+            int i = currentIndex;
+
+            //выделение цветом
+            i = richTextBox1.Text.IndexOf(str, i);
+            richTextBox1.SelectionStart = i;
+            richTextBox1.SelectionLength = str.Length;
+            richTextBox1.SelectionColor = color;
+            richTextBox1.SelectionStart = position_save; // ставим как было
+            richTextBox1.SelectionColor = Color.Black; // чужое красим в черное
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (currentWordIndex < words.Length-1)
+            {
+                currentWord = words[currentWordIndex];
+                ColorWord(currentWord, Color.Red);
+                currentIndex += currentWord.Length;
+                int time = Convert.ToInt32(wordInMinute / currentWord.Length * 1000 * words[currentWordIndex + 1].Length);
+                timer1.Interval = time;
+                currentWordIndex++;
+            }
+            else
+            {
+                isStart = false;
+            }
+        }
+    }
+}
